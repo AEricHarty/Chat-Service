e2111b28c9f0d922bc5bcbf65223493928e21707
Phu Lam <ppham95@uw.edu>
2018-05-02 20:52:14 -0700

    Modified database sql insertion statement with generated verification code and send that code along with the email if register is successful

--- routes/register.js
+++ routes/register.js
@@ -17,6 +17,8 @@ let getHash = require('../utilities/utils').getHash;
 
 let sendEmail = require('../utilities/utils').sendEmail;
 
+let getVerificationCode = require('..utilities/utils').generateVerificationCode;
+
 var router = express.Router();
 
 router.post('/', (req, res) => {
@@ -35,18 +37,19 @@ router.post('/', (req, res) => {
         //watch this youtube video: https://www.youtube.com/watch?v=8ZtInClXe1Q
         let salt = crypto.randomBytes(32).toString("hex");
         let salted_hash = getHash(password, salt);
+        let code = getVerificationCode;
 
         //Use .none() since no result gets returned from an INSERT in SQL
         //We're using placeholders ($1, $2, $3) in the SQL query string to avoid SQL Injection
         //If you want to read more: https://stackoverflow.com/a/8265319
-        let params = [first, last, username, email, salted_hash, salt];
-        db.none("INSERT INTO MEMBERS(FirstName, LastName, Username, Email, Password, Salt) VALUES ($1, $2, $3, $4, $5, $6)", params)
+        let params = [first, last, username, email, salted_hash, salt, code];
+        db.none("INSERT INTO MEMBERS(FirstName, LastName, Username, Email, Password, Salt, VerificationCode) VALUES ($1, $2, $3, $4, $5, $6, $7)", params)
         .then(() => {
             //We successfully added the user, let the user know
             res.send({
                 success: true
             });
-            sendEmail("cfb3@uw.edu", email, "Welcome!", "<strong>Welcome to our app!</strong>");
+            sendEmail("cfb3@uw.edu", email, "Welcome!", "<strong>Welcome to our app!. Please confirm your account by entering your verification code: ${code}.</strong>");
         }).catch((err) => {
             //log the error
             console.log(err);
