d9906a4cf6ea6fee19c5f3681479114ec9998a01
Phu Lam <ppham95@uw.edu>
2018-05-10 13:05:37 -0700

    should fix messages now

--- routes/messages.js
+++ routes/messages.js
@@ -41,12 +41,12 @@ router.get("/getMessages", (req, res) => {
     let chatId = req.query['chatId'];
     let after = req.query['after'];
     let query = `SELECT Members.Username, Messages.Message,
-                 to_char(Messages.Timestamp AT TIME ZONE 'PDT', 'YYYY-MM-DD HH24:MI:SS.US' ) AS Timestamp
+                 to_char(Messages.Timestamp AT TIME ZONE 'PDT', 'YYYY-MM-DD HH24:MI:SS.US' ) AS TimeStamp
                  FROM Messages
                  INNER JOIN Members ON Messages.MemberId=Members.MemberId
                  WHERE ChatId=$2 AND
-                 Timestamp AT TIME ZONE 'PDT' > $1
-                 ORDER BY Timestamp ASC`
+                 TimeStamp AT TIME ZONE 'PDT' > $1
+                 ORDER BY TimeStamp ASC`
     db.manyOrNone(query, [after, chatId])
     .then((rows) => {
         res.send({
