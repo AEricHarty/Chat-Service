948ff3d65edf16b0d5903d183bb54f4d3d51c8ee
AEricHarty <35510883+AEricHarty@users.noreply.github.com>
2018-05-17 21:05:05 -0700

    took out credentials

--- utilities/utils.js
+++ utilities/utils.js
@@ -2,9 +2,7 @@
 let db = require('./sql_conn.js');
 
 //We use this create the SHA256 hash
-// OUR SendGrid log in Credential
-// Username: app95024574@heroku.com
-// Password: af9otsn58431
+
 const crypto = require("crypto");
 const FormData = require("form-data");
 let sendGridAPIKey = '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'
