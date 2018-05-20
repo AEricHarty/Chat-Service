393351487601297509f2340b3d093bbbfa78c444
AEricHarty <35510883+AEricHarty@users.noreply.github.com>
2018-05-19 18:07:23 -0700

    tried making db not const

--- utilities/sql_conn.js
+++ utilities/sql_conn.js
@@ -3,7 +3,7 @@ const pgp = require('pg-promise')();
 pgp.pg.defaults.ssl = true;
 
 //Create connection to Heroku Database
-const db = pgp(process.env.DATABASE_URL);
+db = pgp(process.env.DATABASE_URL);
 
 if(!db) {
    console.log("SHAME! Follow the intructions and set your DATABASE_URL correctly");
