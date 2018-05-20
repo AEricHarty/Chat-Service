a4d198f6250a531b9c26369e795ffea37caead04
AEricHarty <35510883+AEricHarty@users.noreply.github.com>
2018-05-19 18:23:40 -0700

    fixed?

--- utilities/sql_conn.js
+++ utilities/sql_conn.js
@@ -3,9 +3,7 @@ const pgp = require('pg-promise')();
 pgp.pg.defaults.ssl = true;
 
 //Create connection to Heroku Database
-let db;
-//Uncomment next line and change the string to your DATABASE_URL
-db = pgp('postgres://qbchbxubahnjag:ce59d95bf5bf0ae342127c203de81797f0d22572b74157062c8799525b8db035@ec2-174-129-41-64.compute-1.amazonaws.com:5432/d9lb6gviv2jj60');
+let db = pgp(process.env.DATABASE_URL);
 
 if(!db) {
    console.log("SHAME! Follow the intructions and set your DATABASE_URL correctly");
