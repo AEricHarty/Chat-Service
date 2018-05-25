346417d80d69311ab878c201ee14effde0707f10
lloydBrooks <38235703+lloydBrooks@users.noreply.github.com>
2018-05-24 23:17:59 -0700

    added a /getTime end point to get the current timestamp

--- routes/leaveChat.js
+++ routes/leaveChat.js
@@ -39,6 +39,31 @@ router.post("/leaveChat", (req, res) => {
 
 });
 
+router.post("/getTime", (req, res) => {
+    var date = new Date();
+
+    var hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+
+    var min  = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+
+    var sec  = date.getSeconds();
+    sec = (sec < 10 ? "0" : "") + sec;
+
+    var year = date.getFullYear();
+
+    var month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+
+    var day  = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
+
+    var timestamp = year + ":" + month + ":" + day + " " + hour + ":" + min + ":" + sec;
+    res.send({
+        time : timestamp
+    });
+});
 
 router.post("/getMyChats", (req, res) => {
     var username = req.body['username'];
