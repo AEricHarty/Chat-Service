748e22830930c6c9b1649bc3f5161f73948579d7
AEricHarty <35510883+AEricHarty@users.noreply.github.com>
2018-05-27 17:04:28 -0700

    not working

--- routes/weather.js
+++ routes/weather.js
@@ -18,14 +18,15 @@ router.post("/currentgps", (req, res) => {
     var lon = req.body['email'];
     var url = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}`;
     
-    request(url, function (error, response, body) {
-        if (error) {
-            res.send(error);
-        } else {
-            //res.send(body);
-            res.send(response);
-        }
-    });
+    res.send("fail");
+    // request(url, function (error, response, body) {
+    //     if (error) {
+    //         res.send(error);
+    //     } else {
+    //         //res.send(body);
+    //         res.send(response);
+    //     }
+    // });
 });
 
 router.post("/currentzip", (req, res) => {
